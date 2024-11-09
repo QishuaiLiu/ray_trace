15533f67c437e9e2d5a6ecb2052be8e60d8c6161
Qishuai Liu <liuqs1216@gmail.com>
2024-11-10 00:22:13 +0800

    add hittable.

--- main.cpp
+++ main.cpp
@@ -3,19 +3,32 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
+    auto h = dot(r.direction(), oc);
+    // auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+    // auto discriminant = b * b - 4 * a * c;
+    auto discriminant = h * h - a * c;
+    // return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    } else {
+        return (h - std::sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray &r) {
     // return color(0, 0, 0);
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    // if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    //     return color(1, 0, 0);
+    // }
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
@@ -39,7 +52,7 @@ int main(int argc, char **argv) {
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
     auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center = point3(0, 0, 0);
+    const auto camera_center = point3(0, 0, 0);
 
     // cal vector across the horizontal and down the vertical viewport edges
     auto viewport_u = vec3(viewport_width, 0, 0);
