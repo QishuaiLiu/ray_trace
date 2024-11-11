bc3ead430d8e2c921f740942e9e7b2148e353a9a
Qishuai Liu <liuqs1216@gmail.com>
2024-11-11 23:32:59 +0800

    add hittable list

--- main.cpp
+++ main.cpp
@@ -1,38 +1,38 @@
 #include <iostream>
+#include "hittable.h"
+#include "hittable_list.h"
+#include "rtweekend.h"
+#include "sphere.h"
+
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
 
-double hit_sphere(const point3 &center, double radius, const ray &r) {
-    vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto h = dot(r.direction(), oc);
-    // auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius;
-    // auto discriminant = b * b - 4 * a * c;
-    auto discriminant = h * h - a * c;
-    // return (discriminant > 0);
-    if (discriminant < 0) {
-        return -1.0;
-    } else {
-        return (h - std::sqrt(discriminant)) / (2.0 * a);
-    }
-}
+// double hit_sphere(const point3 &center, double radius, const ray &r) {
+//     vec3 oc = center - r.origin();
+//     auto a = dot(r.direction(), r.direction());
+//     auto h = dot(r.direction(), oc);
+//     // auto b = -2.0 * dot(r.direction(), oc);
+//     auto c = dot(oc, oc) - radius * radius;
+//     // auto discriminant = b * b - 4 * a * c;
+//     auto discriminant = h * h - a * c;
+//     // return (discriminant > 0);
+//     if (discriminant < 0) {
+//         return -1.0;
+//     } else {
+//         return (h - std::sqrt(discriminant)) / (2.0 * a);
+//     }
+// }
 
-color ray_color(const ray &r) {
-    // return color(0, 0, 0);
-    // if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-    //     return color(1, 0, 0);
-    // }
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray &r, const hittable &world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.5, 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main(int argc, char **argv) {
@@ -47,6 +47,10 @@ int main(int argc, char **argv) {
 
     // auto viewport_height = 2.0;
     // auto viewport_width = viewport_height * (double(image_width) / image_height);
+    // world
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // camera
     auto focal_length = 1.0;
@@ -90,7 +94,7 @@ int main(int argc, char **argv) {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
