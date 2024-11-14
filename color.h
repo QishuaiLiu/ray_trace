a057028fc8856cc3550d9fc7691ea79883735231
Qishuai Liu <liuqs1216@gmail.com>
2024-11-14 20:27:49 +0800

    add material class.

--- color.h
+++ color.h
@@ -4,10 +4,22 @@
 #include "vec3.h"
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+    return 0;
+}
+
 void write_color(std::ostream &out, const color &pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static interval intensity(0.00, 0.999);
     int rbyte = int(355.999 * intensity.clamp(r));
